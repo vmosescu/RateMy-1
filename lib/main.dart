e93392b0b5bdd9221bac41800138ad87ed32df58
vmosescu <vmosescu@hotmail.com>
2024-12-24 14:42:03 +0200

    added firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:flutter/material.dart';
 import 'dart:io';
 
-import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:ratemy/screens/auth.dart';
+import 'package:ratemy/screens/splash.dart';
+import 'firebase_options.dart';
+
 import 'package:ratemy/screens/profile_screen.dart';
 import 'package:ratemy/screens/feed_screen.dart';
 import 'package:ratemy/screens/login_screen.dart';
@@ -9,8 +15,12 @@ import 'package:ratemy/screens/user_screen.dart';
 
 import 'application/injector.dart';
 
-void main() {
-  HttpOverrides.global = MyHttpOverrides();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  //HttpOverrides.global = MyHttpOverrides();
   runApp(MyApp());
 }
 
@@ -19,46 +29,64 @@ class MyApp extends StatelessWidget {
 
   MyApp({super.key});
 
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
-
     String initialRoute = LoginScreen.id;
 
     return MaterialApp(
       title: 'RateMy',
-      initialRoute: initialRoute,
+      // initialRoute: initialRoute,
       routes: {
-        LoginScreen.id: (context) => LoginScreen(presentation: injector.getLoginPresentation()),
-        UserScreen.id: (context) => UserScreen(presentation: injector.getProfilePresentation(),),
-        ProfileScreen.id: (context) => ProfileScreen(presentation: injector.getProfilePresentation(),),
-        FeedScreen.id: (context) => FeedScreen(presentation: injector.getFeedPresentation(),),
+        LoginScreen.id: (context) =>
+            LoginScreen(presentation: injector.getLoginPresentation()),
+        UserScreen.id: (context) => UserScreen(
+              presentation: injector.getProfilePresentation(),
+            ),
+        ProfileScreen.id: (context) => ProfileScreen(
+              presentation: injector.getProfilePresentation(),
+            ),
+        FeedScreen.id: (context) => FeedScreen(
+              presentation: injector.getFeedPresentation(),
+            ),
       },
       theme: AppTheme.getAppTheme(),
-      onGenerateRoute: (settings) {
-        // All Routes definitions that require parameters
-        if(settings.name == FeedScreen.id) {
-          return MaterialPageRoute(builder: (context) {
-            return FeedScreen(presentation: injector.getFeedPresentation(),);
-          });
-        }
+      // onGenerateRoute: (settings) {
+      //   // All Routes definitions that require parameters
+      //   if(settings.name == FeedScreen.id) {
+      //     return MaterialPageRoute(builder: (context) {
+      //       return FeedScreen(presentation: injector.getFeedPresentation(),);
+      //     });
+      //   }
 
-        return MaterialPageRoute(builder: (context) {
-          return LoginScreen(presentation: injector.getLoginPresentation());
-        });
-      },
+      //   return MaterialPageRoute(builder: (context) {
+      //     return LoginScreen(presentation: injector.getLoginPresentation());
+      //   });
+      // },
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
+          if (snapshot.hasData) {
+            return FeedScreen(
+              presentation: injector.getFeedPresentation(),
+            );
+          }
+          return const AuthScreen();
+        },
+      ),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHttpOverrides extends HttpOverrides{
-
-  @override
-  HttpClient createHttpClient(SecurityContext? context){
-    return super.createHttpClient(context)
-      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
-  }
-}
+// class MyHttpOverrides extends HttpOverrides {
+//   @override
+//   HttpClient createHttpClient(SecurityContext? context) {
+//     return super.createHttpClient(context)
+//       ..badCertificateCallback =
+//           (X509Certificate cert, String host, int port) => true;
+//   }
+// }
